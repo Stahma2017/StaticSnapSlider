a481308ad55f2db04c456895e7be716daeaaca56
Stahma <StasStahma@gmail.com>
2021-05-30 01:45:13 +0300

    thumb drawing added

--- app/src/main/java/ru/myapplication/sdslidersample/ui/custom2/SnapSlider.kt
+++ app/src/main/java/ru/myapplication/sdslidersample/ui/custom2/SnapSlider.kt
@@ -8,11 +8,11 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.ColorInt
 import ru.myapplication.sdslidersample.R
 import ru.myapplication.sdslidersample.dpToPx
-import ru.myapplication.sdslidersample.ui.custom.SizeUtils
 
 class SnapSlider @JvmOverloads constructor(
         context: Context,
@@ -20,16 +20,23 @@ class SnapSlider @JvmOverloads constructor(
 
     @ColorInt
     private val trackColor = context.getColor(R.color.purple_500)
+    private val thumbColor = context.getColor(R.color.teal_200)
 
     private val trackWidth = 20f.dpToPx()
-    private val radius = 6f.dpToPx()
+    private val trackRadius = 10f.dpToPx()
+    private val thumbRadius = 10f.dpToPx()
 
 
     private val trackPaint = Paint().apply {
         isAntiAlias = true
         strokeCap = Paint.Cap.BUTT
         color = trackColor
-        strokeWidth = trackWidth.toFloat()
+    }
+
+    private val thumbPaint = Paint().apply {
+        isAntiAlias = true
+        strokeCap = Paint.Cap.BUTT
+        color = thumbColor
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -37,11 +44,16 @@ class SnapSlider @JvmOverloads constructor(
         drawThumb(canvas)
     }
 
-    private fun drawThumb(canvas: Canvas) {
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return super.onTouchEvent(event)
 
     }
 
+    private fun drawThumb(canvas: Canvas) {
+        canvas.drawCircle(paddingLeft.toFloat() + thumbRadius, paddingTop + (trackWidth/2), thumbRadius, thumbPaint)
+    }
+
     private fun drawTrack(canvas: Canvas) {
-        canvas.drawRoundRect(paddingLeft.toFloat(), paddingTop.toFloat(), paddingRight.toFloat(), paddingTop + trackWidth, radius, radius, trackPaint)
+        canvas.drawRoundRect(paddingLeft.toFloat(), paddingTop.toFloat(), measuredWidth - paddingRight.toFloat(), paddingTop + trackWidth, trackRadius, trackRadius, trackPaint)
     }
 }
